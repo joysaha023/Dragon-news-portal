3c33a8c7d9d54ac363883464de085312f6bef72f
Joy <github.joysaha@gmail.com>
2024-04-03 22:49:59 +0600

    simple route setup

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,10 +1,20 @@
-const { createBrowserRouter } = require("react-router-dom");
+import { createBrowserRouter } from "react-router-dom";
+import Home from "../Pages/Home/Home";
+import Root from "../Layouts/Root/Root";
+
+
 
 
 const router = createBrowserRouter([
     {
       path: "/",
-      element: <div>Hello world!</div>,
+      element: <Root></Root>,
+      children: [
+        {
+            path: "/",
+            element:<Home></Home>
+        }
+      ]
     },
 ]);
 
