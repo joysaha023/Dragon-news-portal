075ae8002a456449bbabfc4183a09faa602609dd
Joy <github.joysaha@gmail.com>
2024-04-04 12:15:24 +0600

    functionality added

--- src/Shared/Header/Header.jsx
+++ src/Shared/Header/Header.jsx
@@ -4,7 +4,7 @@ import moment from 'moment';
 const Header = () => {
     return (
         <div className='text-center'>
-            <img src={logo} alt="" />
+            <img className='mx-auto' src={logo} alt="" />
             <p>Journalism Without Fear or Favour</p>
             <p className='text-xl'>{moment().format("dddd, MMMM D, YYYY")}</p>
         </div>
