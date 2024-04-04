c8008c39b6f671f5a741e21678beafc57c3dc00f
Joy <github.joysaha@gmail.com>
2024-04-04 17:43:35 +0600

    bg section added with button

--- src/Shared/RightSideNav/RightSideNav.jsx
+++ src/Shared/RightSideNav/RightSideNav.jsx
@@ -1,5 +1,15 @@
 import React from "react";
-import { FaFacebook, FaGithub, FaGoogle, FaInstagram, FaTwitter } from "react-icons/fa6";
+import {
+  FaFacebook,
+  FaGithub,
+  FaGoogle,
+  FaInstagram,
+  FaTwitter,
+} from "react-icons/fa6";
+import Qzone from '.././../assets/qZone1.png';
+import Qzone2 from '.././../assets/qZone2.png';
+import Qzone3 from '.././../assets/qZone3.png';
+import bgAd from '../../assets/bg.png'
 
 const RightSideNav = () => {
   return (
@@ -17,9 +27,37 @@ const RightSideNav = () => {
       </div>
       <h2 className="text-lg font-bold mb-4">Find Us On</h2>
       <div>
-        <a href="" className="p-4 flex items-center gap-3 border text-sm rounded-t-lg"><FaFacebook /> Facebook</a>
-        <a href="" className="p-4 flex items-center gap-3 border text-sm rounded-x-lg"><FaTwitter /> Twitter</a>
-        <a href="" className="p-4 flex items-center gap-3 border text-sm rounded-b-lg"><FaInstagram /> Instagram</a>
+        <a
+          href=""
+          className="p-4 flex items-center gap-3 border  hover:text-blue-400 text-sm rounded-t-lg"
+        >
+          <FaFacebook /> Facebook
+        </a>
+        <a
+          href=""
+          className="p-4 flex items-center gap-3  hover:text-blue-400 border text-sm rounded-x-lg"
+        >
+          <FaTwitter /> Twitter
+        </a>
+        <a
+          href=""
+          className="p-4 flex items-center gap-3 border hover:text-blue-400 text-sm rounded-b-lg"
+        >
+          <FaInstagram /> Instagram
+        </a>
+      </div>
+      <div className="bg-gray-200 p-2 rounded-md my-5">
+        <h2 className="text-lg font-bold p-3">Q-Zone</h2>
+        <div className="space-y-4 mb-2">
+            <img src={Qzone} alt="" />
+            <img src={Qzone2} alt="" />
+            <img src={Qzone3} alt="" />
+        </div>
+      </div>
+      <div style={{backgroundImage:`url(${bgAd})`}} className="text-center space-y-4 p-10 text-white">
+        <h2 className="text-[30px] font-bold">Create an Amazing Newspaper</h2>
+        <p>Discover thousands of options, easy to customize layouts, one-click to import demo and much more.</p>
+        <button className="btn btn-error text-white">Learn More</button>
       </div>
     </div>
   );
