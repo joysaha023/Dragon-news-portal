6939b5c96e1144a8790462b6555e1ebb659c0ec4
Joy <github.joysaha@gmail.com>
2024-04-04 09:38:08 +0600

    logo add and dynamically time zone added

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,13 +1,26 @@
 import React from 'react';
 import Header from '../../Shared/Header/Header';
 import NavBar from '../../Shared/NavBar/NavBar';
+import LeftSideNav from '../../Shared/LeftSideNav/LeftSideNav';
+import RightSideNav from '../../Shared/RightSideNav/RightSideNav';
 
 const Home = () => {
     return (
         <div>
             <Header></Header>
             <NavBar></NavBar>
-            <h2 className=''>Hello guys</h2>
+            <h2 className=''>This is home</h2>
+            <div className='grid grid-cols-1 md:grid-cols-4 gap-6'>
+                <div className='bg-gray-300 '>
+                    <LeftSideNav></LeftSideNav>
+                </div>
+                <div className='lg:col-span-2 bg-gray-300'>
+                    <h2 className='text-4xl'>News comming soon</h2>
+                </div>
+                <div className='bg-gray-300'>
+                    <RightSideNav></RightSideNav>
+                </div>
+            </div>
         </div>
     );
 };
--- src/Shared/Header/Header.jsx
+++ src/Shared/Header/Header.jsx
@@ -1,9 +1,12 @@
-import React from 'react';
+import logo from '../../assets/logo.png'
+import moment from 'moment';
 
 const Header = () => {
     return (
         <div>
-            <h2>This is header</h2>
+            <img src={logo} alt="" />
+            <p>Journalism Without Fear or Favour</p>
+            <p className='text-xl'>{moment().format("dddd, MMMM D, YYYY")}</p>
         </div>
     );
 };
